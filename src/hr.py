8f7b3503bf2febda5335c8eeb0d0b3db5b6367e6
Samariddin Kushmuratov <bekmezonali@gmail.com>
2022-12-26 21:54:35 +0500

    environment boyicha ustanovka ishladi va face detectionni qoshidim

--- src/hr.py
+++ src/hr.py
@@ -1,17 +1,29 @@
-class HR:
-    def __init__(self):
-        print("HR classga bo'glandi!")
+# import cv2
+from insightface.app import FaceAnalysis
 
-    def detection(self):
+
+app = FaceAnalysis(allowed_modules=["detection"])
+app.prepare(ctx_id=0, det_size=(640, 640), det_thresh=0.7)
+
+
+class HR:
+    def __int__(self):
+        pass
+
+    def detection(self, img):
         print("detection f-ya")
+        faces = app.get(img)
+        for face in faces:
+            print(face.bbox)
+            # rasm koordinatalari shu yerda yuboriladi
 
     def agegender(self):
         print("age-gender f-ya")
 
-    def recognition(self):
-        print("recognition f-ya")
+    def recognation(self):
+        print("recognation f-ya")
+
 
 # myHR = HR()
-# myHR.detection()
 # myHR.agegender()
 # myHR.recognation()
