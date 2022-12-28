f0adeadad2b064ec03085db3b9f982d07bf567f9
Samariddin Kushmuratov <bekmezonali@gmail.com>
2022-12-29 00:12:37 +0500

    hr-5-detection-test natijalari qoshildi

--- client-api/clientapp.py
+++ client-api/clientapp.py
@@ -1,7 +1,7 @@
 from src.hr import HR
 import cv2
 
-_path = "../data/drive/rasmlar_chiqish/1_1_1_2022-10-09-17-37-44.jpg"
+_path = "../data/drive/rasmlar_chiqish/1_1_1_2022-10-09-17-38-10.jpg"
 img = cv2.imread(_path)
 myHR = HR()
 faces = myHR.detection(img)
--- src/hr.py
+++ src/hr.py
@@ -1,9 +1,10 @@
 import cv2
 from insightface.app import FaceAnalysis
-
+import os
+import uuid
 
 app = FaceAnalysis(allowed_modules=["detection"])
-app.prepare(ctx_id=0, det_size=(640, 640), det_thresh=0.7)
+app.prepare(ctx_id=0, det_size=(640, 640), det_thresh=0.3)
 
 
 class HR:
@@ -22,11 +23,17 @@ class HR:
             y2 = int(face["bbox"][3])
             crop_img = img[y1:y2, x1:x2]
             print(crop_img.shape)
+            # test uchin ishlatildi
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
             # test papka yaratib osha joyga detection bogan rasmlarni saqlaymiz
             # cv2.imwrite("detect.jpg",crop_img)
 
             # rasm koordinatalari shu yerda yuboriladi
             all_detect_facees.append([face.bbox, face.kps, crop_img.shape])
+
+        # test qilish uchun ishlatildi
+        # full_filename = os.path.join("../tests/detection/", "test-det_score-0,3-"+str(uuid.uuid4())+".jpg")
+        # cv2.imwrite(full_filename, img)
 
         return all_detect_facees
 
