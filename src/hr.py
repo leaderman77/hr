373a3223412fde42d3442b818fa33d223bdc7bfa
Samariddin Kushmuratov <bekmezonali@gmail.com>
2022-12-30 13:52:42 +0500

    #5-detection test boyicha togirlashlar bajarildi

--- src/hr.py
+++ src/hr.py
@@ -1,28 +1,21 @@
-import cv2
+# import cv2
 from insightface.app import FaceAnalysis
-import os
-import uuid
-
-app = FaceAnalysis(allowed_modules=["detection"])
-app.prepare(ctx_id=0, det_size=(640, 640), det_thresh=0.3)
 
 
 class HR:
-    def __int__(self):
-        pass
+    def __int__(self, module="detection", det_size=(640, 640), det_thresh=0.3):
+        self.app = FaceAnalysis(allowed_modules=[module])
+        self.app.prepare(ctx_id=0, det_size=det_size, det_thresh=det_thresh)
 
     def detection(self, img):
-        faces = app.get(img)
+        faces = self.app.get(img)
         all_detect_faces = []
         for face in faces:
-            print(face.bbox)
-            print(face.kps)
             x1 = int(face["bbox"][0])
             y1 = int(face["bbox"][1])
             x2 = int(face["bbox"][2])
             y2 = int(face["bbox"][3])
             crop_img = img[y1:y2, x1:x2]
-            print(crop_img.shape)
 
             # rasm koordinatalari shu yerda aniqlanadi
             all_detect_faces.append([face.bbox, face.kps, crop_img.shape])
--- tests/detection/test.py
+++ tests/detection/test.py
@@ -1,12 +1,16 @@
 import cv2
 from src.hr import HR
+import glob
+import os
+import uuid
 
 hr = HR()
+imgs_path = "../../data/drive/rasmlar_kirish/*.jpg"
 
-for img_path in imgs_path:
+for img_path in glob.glob(imgs_path):
     img = cv2.imread(img_path)
     det_data = hr.detection(img)
- 
+
     for data in det_data:
         bbox, kps, _shape = data
         x1 = int(bbox[0])
@@ -15,7 +19,8 @@ for img_path in imgs_path:
         y2 = int(bbox[3])
         # draw bbox
         cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-        # draw kps 
+        # draw kps
         # please put here
-    filename = ...
+    filename = os.path.join("", "test-det_score-" + str(uuid.uuid4()) + ".jpg")
+    print(filename)
     cv2.imwrite(filename, img)
