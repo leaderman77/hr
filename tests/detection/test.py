523ee94355052d4c104ad8f9eba285fe0a21732f
Samariddin Kushmuratov <bekmezonali@gmail.com>
2022-12-30 13:12:38 +0500

    Update tests/detection/test.py
    zor
    
    Co-authored-by: Elyor Kodirov <elyor.kodirov@neochromosome.com>

--- tests/detection/test.py
+++ tests/detection/test.py
@@ -1,53 +1,21 @@
 import cv2
-from insightface.app import FaceAnalysis
-import os
-import uuid
+from src.hr import HR
 
-app = FaceAnalysis(allowed_modules=["detection"])
-app.prepare(ctx_id=0, det_size=(640, 640), det_thresh=0.3)
+hr = HR()
 
-
-class HR:
-    def __int__(self):
-        pass
-
-    def detection(self, img):
-        faces = app.get(img)
-        all_detect_faces = []
-        for face in faces:
-            print(face.bbox)
-            print(face.kps)
-            x1 = int(face["bbox"][0])
-            y1 = int(face["bbox"][1])
-            x2 = int(face["bbox"][2])
-            y2 = int(face["bbox"][3])
-            crop_img = img[y1:y2, x1:x2]
-            print(crop_img.shape)
-            # test uchin ishlatildi
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-
-            # rasm koordinatalari shu yerda yuboriladi
-            all_detect_faces.append([face.bbox, face.kps, crop_img.shape])
-
-        # test qilish uchun ishlatildi
-        full_filename = os.path.join(
-            "../tests/detection/", "test-det_score-0,3-" + str(uuid.uuid4()) + ".jpg"
-        )
-        cv2.imwrite(full_filename, img)
-
-        return all_detect_facees
-
-    def agegender(self):
-        print("age-gender f-ya")
-
-    def embeding(self):
-        print("embeding f-ya")
-
-
-_path = "../data/drive/rasmlar_chiqish/1_1_1_2022-10-09-17-38-10.jpg"
-img = cv2.imread(_path)
-myHR = HR()
-faces = myHR.detection(img)
-
-# myHR.agegender()
-# myHR.recognation()
+for img_path in imgs_path:
+    img = cv2.imread(img_path)
+    det_data = hr.detection(img)
+ 
+    for data in det_data:
+        bbox, kps, _shape = data
+        x1 = int(bbox[0])
+        y1 = int(bbox[1])
+        x2 = int(bbox[2])
+        y2 = int(bbox[3])
+        # draw bbox
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        # draw kps 
+        # please put here
+    filename = ...
+    cv2.imwrite(filename, img)
