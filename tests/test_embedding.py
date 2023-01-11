2e9eaaee1967e943e9425ec98e84ae0e66504c59
leaderman77 <sardorasatillaev@gmail.com>
2023-01-11 23:25:19 +0500

    #24 docstrings and code modifications

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -1,51 +1,46 @@
 import cv2
-from src.hr import HR
 import numpy as np
+from src.hr import HR
 
 def test_embedding():
     """
-    Aniqlashngan yuzdan vektor korinishidagi embedding olinishini test qilish:
-        - berilgan path bo'yicha 3ta .jpg rasmlarni load qiladi
-        - Har bir rasm boyicha xar bir yuz uchun yuz embedding olinadi,
-        - Toplangan embeddinglarni solishtiramiza. Bunda rasm_1 va rasm_2 bitta odam va
-          embedding solishtirsak ular yaqin bo'lish kerak rasm_3 ga nisbatan.
+    To test to get face embeddings in 512-d numerical vectors obtained from a defined face:
+         - loads 3 .jpg images according to the given path
+         - Face embedding is obtained for each detected face on each image,
+         - Compare defined face embeddings. Picture_1 and picture_2 are
+           images of the same person. picture_3 is an image of third person.
+           In comparision, picture_1 and picture_2 should be close to each other
+           but different from picture_3.
     """
     hr = HR()
     img1_path = "../data/drive/rasmlar_kirish/1_1_1_2022-10-09-17-32-26.jpg"
     img2_path = "../data/drive/rasmlar_chiqish/1_1_1_2022-10-09-17-38-10.jpg"
     img3_path = "../data/s3/merchant_1/location_1/camera_1/2022-11-02/2022-11-02 18_59_59.jpg"
-    imgPath_list = [img1_path, img2_path, img3_path]
-
-    output_img_list = []
-
-    for img_path in imgPath_list:
-        img = cv2.imread(img_path)
-        embedding_data = hr.embeding(img)
-        output_img_list.append(embedding_data[0])
-    print(output_img_list)
 
 
     img1 = cv2.imread(img1_path)
-    feat1 = hr.embeding(img1)
-    feat1 = feat1[0]
+    feat_1_ruzmat = hr.embeding(img1)
+    feat_1_ruzmat = feat_1_ruzmat[0][0].embedding
 
     img2 = cv2.imread(img2_path)
-    feat2 = hr.embeding(img2)
-    feat2 = feat2[0]
+    feat_2_ruzmat = hr.embeding(img2)
+    feat_2_ruzmat = feat_2_ruzmat[0][0].embedding
 
     img3 = cv2.imread(img3_path)
-    feat3 = hr.embeding(img3)
-    feat3 = feat3[0]
+    feat_3_samariddin = hr.embeding(img3)
+    feat_3_samariddin = feat_3_samariddin[0][0].embedding
 
-    sim = compute_sim(feat2, feat3)
-    if sim < 0.2:
-        conclu = 'They are NOT the same person'
-    elif sim >= 0.2 and sim < 0.28:
-        conclu = 'They are LIKELY TO be the same person'
+    sim_same = compute_sim(feat_1_ruzmat, feat_2_ruzmat)
+    sim_diff = compute_sim(feat_1_ruzmat, feat_3_samariddin)
+
+    assert sim_same > sim_diff
+
+    if sim_same > sim_diff:
+        print("Correct")
+        print("{} > {}".format(sim_same, sim_diff))
     else:
-        conclu = 'They ARE the same person'
-    print(sim)
-    print(conclu)
+        print("Incorrect")
+        print("{} > {}".format(sim_same, sim_diff))
 
 
 def compute_sim(feat1, feat2):
