e40d644c1aa9f0602558b1c641740d4dd47b39d6
orifxolbekov <xolbekovo170@gmail.com>
2023-01-26 17:44:59 +0500

    hr-19-cod va jadvalga tuldirish kiritildi

--- src/s3_code.py
+++ src/s3_code.py
@@ -15,14 +15,17 @@ def fayl_ichidagi_elementlar_soni(asosiy_joy):
     -------
     fayllar ichidagi elementlar soni
     """
-    s = 0
+
     folders = os.listdir(asosiy_joy)
     for folder in folders:
+        k = 0
         camera = os.path.join(asosiy_joy, folder)
         print(folder)
         fayllar = os.listdir(camera)
         # harb bir kamera ichiga kirish
         for _joy_fayl in fayllar:
+            s = 0
+
             if _joy_fayl[-5:] != "yomon":
                 _joy2 = os.path.join(camera, _joy_fayl)
                 _joy2_fayllar = os.listdir(_joy2)
@@ -30,7 +33,9 @@ def fayl_ichidagi_elementlar_soni(asosiy_joy):
                 for _joy2_fayl in _joy2_fayllar:
                     _joy3 = os.path.join(_joy2, _joy2_fayl)
                     s = s + 1
+                    k = k + 1
                 print(_joy_fayl, "da", s, "ta rasm bor")
+        print(folder, "da", k, "ta rasm bor")
 
 
 asosiy_joy = os.path.join(
