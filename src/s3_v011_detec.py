a8c78dde8768883218e78a6818cd6490c3b1c2a4
orifxolbekov <xolbekovo170@gmail.com>
2023-01-30 03:23:01 +0500

    hr-19-tuldirish kiritildi

--- src/s3_v011_detec.py
+++ src/s3_v011_detec.py
@@ -27,29 +27,38 @@ def det():
         "merchant_1",
         "location_1",
     )
-    for img_path in glob.glob(imgs_path):
-        img_path = "/home/ubuntuuser/proyektlar/hr/data/s3_v0.1.1/merchant_1/location_1/camera_1/2022-11-02/2022-11-02 18_58_54.jpg"
-        img = cv2.imread(img_path)
-        print(img)
-        det_data = hr.detection(img)
 
-        for data in det_data:
-            bbox, kps, _shape = data
-            x1 = int(bbox[0])
-            y1 = int(bbox[1])
-            x2 = int(bbox[2])
-            y2 = int(bbox[3])
+    folders = os.listdir(imgs_path)
+    for folder in folders:
+        camera = os.path.join(imgs_path, folder)
+        fayllar = os.listdir(camera)
+        for _joy_fayl in fayllar:
+            _joy2 = os.path.join(camera, _joy_fayl)
+            _joy2_fayllar = os.listdir(_joy2)
+            for _joy2_fayl in _joy2_fayllar:
+                _joy3 = os.path.join(_joy2, _joy2_fayl)
+                img_path = _joy3
+                img = cv2.imread(img_path)
+                print(img)
+                det_data = hr.detection(img)
 
-            # draw bbox
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
-            # print(bbox)
+                for data in det_data:
+                    bbox, kps, _shape = data
+                    x1 = int(bbox[0])
+                    y1 = int(bbox[1])
+                    x2 = int(bbox[2])
+                    y2 = int(bbox[3])
 
-            # draw kps
-            if kps is not None:
-                kps = kps.astype(np.int_)
-                for l in range(kps.shape[0]):
-                    cv2.circle(img, (kps[l][0], kps[l][1]), 5, (0, 0, 255), -1)
-            cv2.imwrite(nom_uzgartir(img_path), img)
+                    # draw bbox
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                    # print(bbox)
+
+                    # draw kps
+                    if kps is not None:
+                        kps = kps.astype(np.int_)
+                        for l in range(kps.shape[0]):
+                            cv2.circle(img, (kps[l][0], kps[l][1]), 5, (0, 0, 255), -1)
+                    cv2.imwrite(nom_uzgartir(img_path), img)
 
 
 det()
