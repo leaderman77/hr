5077d40c1d9d0333971d99a760a375e428f70ac0
Samariddin Kushmuratov <bekmezonali@gmail.com>
2023-02-02 21:50:56 +0500

    hr-63:byte ko'rinishdagi rasmni qaytadan rasmga aylantirib undan yuzlarni topib, har bir yuzni featurelari aniqlandi

--- src/image2obj.py
+++ src/image2obj.py
@@ -1,14 +1,22 @@
+import os
+import io
 import cv2
+import json
+import base64
 import jsonpickle
+from PIL import Image
 
+from utils import project_dir
 from hr import HR
 
+PROJECT_DIR = project_dir()
 app = HR(option_list=["all"])
 
 
-def img_convertor(img_path):
+def img_convertor(img_byte):
     """
-    frame pathi keladi va undan  yuzlarni qirqib olib embedding, age va genderni aniqlab json ko'rinishida natija sifatida qaytaradi.
+    rasmni byte ko'rinishida keladi, rasmga decode qilinadi va undan  yuzlarni qirqib olib embedding, age va genderni
+     aniqlab json ko'rinishida natija sifatida qaytaradi.
     Parameters
     ----------
     img_path: str
@@ -18,7 +26,9 @@ def img_convertor(img_path):
     facedata: json
     """
 
-    img = cv2.imread(img_path)
+    img = Image.open(io.BytesIO(base64.decodebytes(bytes(img_byte, "utf-8"))))
+    img.save("dummy_img.jpg")
+    img = cv2.imread("dummy_img.jpg")
     faces = app.get_face_data(img)
     facedata = {}
     for index, face in enumerate(faces):
@@ -29,9 +39,14 @@ def img_convertor(img_path):
         obj["age"] = age
         obj["img_obj"] = jsonpickle.encode(bbox.tolist())
         facedata[str(index)] = obj
-    return facedata
+    json_data = json.dumps(facedata)
+    os.remove("dummy_img.jpg")
+
+    return json_data
 
 
-# s3 dagi rasm manizli
-_path = "../tests/embedding/2022-11-02 18_59_59.jpg"
-img_convertor(_path)
+# s3 dagi rasm manzili yoki local manzil
+_path = os.path.join(PROJECT_DIR, "tests", "embedding", "2022-11-02 18_59_59.jpg")
+image = cv2.imread(_path)
+_, img_encoded = cv2.imencode(".jpg", image)
+img_convertor(str(base64.encodebytes(img_encoded), "utf-8"))
